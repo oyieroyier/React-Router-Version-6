9c700beea5791cdbe314d08bfdbdce8a24294e0e
Bob Oyier <boboyier92@gmail.com>
2023-03-19 01:04:02 +0300

    Completed the tutorial.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Link, Navigate, Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
 
 // pages
-import Home from './pages/Home'
-import About from './pages/About'
-import Products from './pages/Products'
-import ProductDetails from './pages/ProductDetails'
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <nav>
-          <h1>The Ninja Clothing Company</h1>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/products">Products</Link>
-        </nav>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/products/:id">
-            <ProductDetails />
-          </Route>
-          <Route path="/products">
-            <Products />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  )
+	const [cartIsEmpty] = useState(true);
+
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<nav>
+					<h1>The Ninja Clothing Company</h1>
+					<Link to="/">Home</Link>
+					<Link to="/about/">About</Link>
+					<Link to="/products">Products</Link>
+				</nav>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about/*" element={<About />} />
+					<Route path="/products/:id" element={<ProductDetails />} />
+					<Route path="/products/*" element={<Products />} />
+					<Route
+						path="/test"
+						element={
+							<div>
+								<h1>Hello, from Test!!</h1>
+							</div>
+						}
+					/>
+					<Route path="/redirect" element={<Navigate to="/test" />} />
+					<Route
+						path="/checkout"
+						element={
+							!cartIsEmpty ? <Navigate to="/products" /> : <p> Checkout</p>
+						}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
-export default App
+export default App;
